e4e2d649cb07445a364ff2129fcf07cc703f4ee2
zimmy-upstage <kpdpkp2@gmail.com>
2023-01-17 22:44:20 +0900

    ref: request when checking

--- streamlit/demo.py
+++ streamlit/demo.py
@@ -4,9 +4,9 @@ import streamlit as st
 
 user_id = st.number_input("User ID", min_value=0, max_value=100, value=10)
 age = st.number_input("Age", min_value=0, max_value=100, value=10)
-res = requests.api.post(
-    "http://localhost:8000/recommend", json={"user_id": user_id, "age": age}
-)
 
 if st.checkbox("Show response"):
+    res = requests.api.post(
+        "http://localhost:8000/recommend", json={"user_id": user_id, "age": age}
+    )
     st.json(res.json())
