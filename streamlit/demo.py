229736ef4cce66e863a33b5532ec6a1d3da97da7
ZiminPark <kpdpkp@gmail.com>
2023-01-18 21:28:05 +0900

    feat: use redis for feature retrieval

--- streamlit/demo.py
+++ streamlit/demo.py
@@ -2,6 +2,7 @@ import os
 
 import requests
 from dotenv import load_dotenv
+from redis import Redis
 
 import streamlit as st
 
@@ -16,13 +17,17 @@ def get_url() -> str:
 
 load_dotenv()
 backend_url = get_url()
+r = Redis(host=os.environ["REDIS_ADDRESS"], port=int(os.environ["REDIS_PORT"]))
 
 
-user_id = st.number_input("User ID", min_value=0, max_value=100, value=10)
-age = st.number_input("Age", min_value=0, max_value=100, value=10)
+user_id = st.number_input("userId", min_value=0, max_value=100, step=1)
 
-if st.checkbox("Show response"):
+
+if st.checkbox("Show response for selected user"):
+    age = r.get(f"sample:feature:userId:{user_id}:age").decode("utf-8")
     res = requests.api.post(
         f"{backend_url}/recommend", json={"user_id": user_id, "age": age}
     )
+    st.text(f"User ID: {user_id}")
+    st.text(f"Age: {age}")
     st.json(res.json())
