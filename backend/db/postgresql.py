2516b6c33c69099adbe8839a20d40f167f28d653
ZiminPark <kpdpkp@gmail.com>
2023-01-22 23:37:17 +0900

    update: use mongo instead of postgresql

--- backend/db/postgresql.py
+++ backend/db/postgresql.py
@@ -1,6 +1,3 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from pymongo import MongoClient
 
-URL = "postgresql://아이디:비밀번호@서버주소/DB이름"
-engine = create_engine(URL, echo=False)
-SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
+client = MongoClient(host="localhost", port=27017)
